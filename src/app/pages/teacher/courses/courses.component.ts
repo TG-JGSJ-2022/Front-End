6c0472e45fff77c4fdedabebeb158b4b244bdc01
Juan Diego Campos Neira <juand.campos@javeriana.edu.co>
2022-03-28 15:37:59 -0500

    Course cards created

--- src/app/pages/teacher/courses/courses.component.ts
+++ src/app/pages/teacher/courses/courses.component.ts
@@ -9,7 +9,43 @@ export class CoursesComponent implements OnInit {
 
   constructor() { }
 
+  courses: Array<Object> = [];
+
+  // TODO :
+  // This function should call the backend in order 
+  // to fill the array of courses, but at this point the 
+  // courses will be mocked
+  fillTeacherCoursesArray(): void {
+    this.courses = [
+      {
+        courseCode: "15160", 
+        courseName: "Visualización de datos",
+      },
+      {
+        courseCode: "15160", 
+        courseName: "Introducción a la inteligencia artificial",
+      },
+      {
+        courseCode: "15160",
+        courseName: "Análisis de algoritmos",
+      },
+      {
+        courseCode: "15160", 
+        courseName: "Visualización de datos",
+      },
+      {
+        courseCode: "15160", 
+        courseName: "Introducción a la inteligencia artificial",
+      },
+      {
+        courseCode: "15160", 
+        courseName: "Análisis de algoritmos",
+      }
+    ];
+  } 
+
   ngOnInit(): void {
+    this.fillTeacherCoursesArray();
   }
 
 }
