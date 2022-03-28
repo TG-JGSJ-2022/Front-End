d26bea017bffde1dc260f8519a46dfef8e2d7ba0
Juan Diego Campos Neira <juand.campos@javeriana.edu.co>
2022-03-28 10:18:50 -0500

    Courses component initialized

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/shared/login/login.component';
 import { VideoCaptureComponent } from './pages/student/video-capture/video-capture.component';
+import { CoursesComponent } from './pages/teacher/courses/courses.component';
 
 const routes: Routes = [
   { path: 'home', component: VideoCaptureComponent },
-  { path: 'login', component: LoginComponent }
+  { path: 'login', component: LoginComponent }, 
+  { path: 'courses', component: CoursesComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { VideoCaptureComponent } from './pages/student/video-capture/video-capture.component';
 import { LoginComponent } from './pages/shared/login/login.component';
+import { CoursesComponent } from './pages/teacher/courses/courses.component';
 
 import { AppRoutingModule } from './app-routing.module';
 import { WebcamModule } from 'ngx-webcam';
@@ -15,7 +16,8 @@ import { HttpClientModule } from '@angular/common/http';
   declarations: [
     AppComponent,
     LoginComponent,
-    VideoCaptureComponent
+    VideoCaptureComponent,
+    CoursesComponent
   ],
   imports: [
     BrowserModule,
