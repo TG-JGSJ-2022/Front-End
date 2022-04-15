fd24b95e1c0cd1e2dcbf3daba8a411c0a1665071
Juan Diego Campos Neira <juand.campos@javeriana.edu.co>
2022-04-15 15:53:28 -0500

    Login page finished

--- src/app/pages/shared/login/login.component.ts
+++ src/app/pages/shared/login/login.component.ts
@@ -1,5 +1,5 @@
-import { HttpClient, HttpHeaders, HttpParams } from '@angular/common/http';
-import { Component, Input, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { UserServiceService } from 'src/app/services/user/user-service.service';
 
@@ -11,22 +11,29 @@ import { UserServiceService } from 'src/app/services/user/user-service.service';
 })
 export class LoginComponent implements OnInit {
 
-  username: string = ""; 
-  password: string = "";
+  username: string = ''; 
+  password: string = '';
 
-  constructor(private http: HttpClient, 
-              private router: Router, 
+  universityLogo: string = '/assets/images/PujLogo.svg';
+  userLogo: string = '/assets/images/userLogo.svg'; 
+  passwordLogo: string = '/assets/images/passwordLogo.svg';
+
+  constructor(private router: Router, 
               private userService: UserServiceService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    if (sessionStorage.getItem('user')) {
+      console.log("User already sign in");
+    }
+  }
 
   submitForm(event: any) {
     this.userService.login(this.username, this.password)
-      .subscribe( data => {
+      .subscribe( (data) => {
         sessionStorage.setItem('user', this.username);
         this.router.navigate(["/courses"]);
         console.log(data);
-      }, error => {
+      }, (error) => {
         this.router.navigate(["/login"]);
       });
   }
