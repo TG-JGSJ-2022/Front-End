d35def5fda1abb15ce328f701c4ec5c6f9b76b93
Juan Diego Campos Neira <juand.campos@javeriana.edu.co>
2022-04-24 20:48:28 -0500

    Sessions route added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { LoginComponent } from './pages/shared/login/login.component';
 import { VideoCaptureComponent } from './pages/student/video-capture/video-capture.component';
 import { CoursesComponent } from './pages/teacher/courses/courses.component';
 import { RealTimeEmotionsComponent } from './pages/teacher/real-time-emotions/real-time-emotions.component';
+import { SessionsComponent } from './pages/teacher/sessions/sessions.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent },
   // Teacher routes
   { path: 'courses', component: CoursesComponent },
   { path: 'modal', component: RealTimeEmotionsComponent}, 
+  { path: 'sessions', component: SessionsComponent}, 
   // Student routes
   { path: 'capture', component: VideoCaptureComponent },
 ];
