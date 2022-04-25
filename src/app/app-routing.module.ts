e51cd47099c8ce9cfb26ef0bc565db72927c280b
unknown <gabrielamcamacho@javerian.edu.co>
2022-04-25 17:19:04 -0500

    Pantalla de compromiso profesor perfecta

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './pages/shared/login/login.component';
 import { VideoCaptureComponent } from './pages/student/video-capture/video-capture.component';
 import { CoursesComponent } from './pages/teacher/courses/courses.component';
+import { AgreementComponent } from './pages/teacher/agreement/agreement.component';
 
 const routes: Routes = [
   { path: 'capture', component: VideoCaptureComponent },
   { path: 'login', component: LoginComponent },
   { path: 'courses', component: CoursesComponent },
+  { path: 'agreement', component:AgreementComponent}
 ];
 
 @NgModule({
