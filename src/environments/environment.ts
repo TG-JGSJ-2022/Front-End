be6b5e1d1ac9d1b14cc2e0c0c3afb0cbeeeae086
Julian Camilo Builes Serrano <buiser147@hotmail.com>
2022-05-11 10:56:06 -0500

    cambios

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,6 +5,7 @@
 const port = '5000';
 const NEURAL_NETWORK_PORT = '8080';
 const url = "http://servidorflaskprueba-env-1.eba-2wecp7sf.us-east-1.elasticbeanstalk.com"
+// const url = "http://localhost:5000"
 export const environment = {
   production: false,
   model_endpoint: `${url}/recibir-imagen`,
@@ -12,8 +13,10 @@ export const environment = {
   api_login: `${url}/login`,
   api_logout: `${url}/logout`,
   api_courses: `${url}/courses`,
+  api_course_sessions: `${url}/course-sessions`,
   api_sesion_data : `${url}/info_sesion`
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
