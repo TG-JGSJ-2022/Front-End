c7eba0f848180a57f84ad6ed3b13d7550f577062
Julian Camilo Builes Serrano <buiser147@hotmail.com>
2022-05-12 10:59:28 -0500

    cambios de linea

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 const port = '5000';
 const NEURAL_NETWORK_PORT = '8080';
-const url = "http://servidorflaskprueba-env-1.eba-2wecp7sf.us-east-1.elasticbeanstalk.com"
-// const url = "http://localhost:5000"
+// const url = "http://servidorflaskprueba-env-1.eba-2wecp7sf.us-east-1.elasticbeanstalk.com"
+const url = "http://localhost:5000"
 export const environment = {
   production: false,
   model_endpoint: `${url}/recibir-imagen`,
